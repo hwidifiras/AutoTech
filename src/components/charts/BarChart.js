9ce734b95a5e383701ee641d021accb0e5c9c2ee
hwidifiras <hwidifiras@gmail.com>
2025-12-05 23:37:58 +0100

    pushing template

---
+++ src/components/charts/BarChart.js
@@ -0,0 +1,52 @@
+import React, { Component } from "react";
+import Chart from "react-apexcharts";
+
+class ColumnChart extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      chartData: props.chartData || [],
+      chartOptions: props.chartOptions || {},
+    };
+  }
+
+  componentDidMount() {
+    this.setState({
+      chartData: this.props.chartData || [],
+      chartOptions: this.props.chartOptions || {},
+    });
+  }
+
+  componentDidUpdate(prevProps) {
+    if (
+      prevProps.chartData !== this.props.chartData ||
+      prevProps.chartOptions !== this.props.chartOptions
+    ) {
+      this.setState({
+        chartData: this.props.chartData || [],
+        chartOptions: this.props.chartOptions || {},
+      });
+    }
+  }
+
+  render() {
+    const { chartData, chartOptions } = this.state;
+    
+    // Don't render if no data is available
+    if (!chartData || chartData.length === 0) {
+      return null;
+    }
+
+    return (
+      <Chart
+        options={chartOptions}
+        series={chartData}
+        type='bar'
+        width='100%'
+        height='100%'
+      />
+    );
+  }
+}
+
+export default ColumnChart;
